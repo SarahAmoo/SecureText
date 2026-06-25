054fbe92e8bd0e6628d95d6a69b782b4c6fe3505
SarahAmoo <sarah@example.com>
2026-06-25 17:34:40 +0000

    Add gunicorn, prepare for Render deployment

--- app.py
+++ app.py
@@ -55,9 +55,6 @@ def cipher():
 
 
 if __name__ == "__main__":
-    import sys
-    if "--prod" in sys.argv:
-        from waitress import serve
-        serve(app, host="0.0.0.0", port=5000)
-    else:
-        app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
